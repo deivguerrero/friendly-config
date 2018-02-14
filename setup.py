0efec7d406f5aebf6f57005ff540ebf67ec798fc
David Guerrero Morales <deivguerrero@gmail.com>
2018-02-14 14:44:31 -0600

    ✅ only test

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'mypackage',
+  packages = ['mypackage'], # this must be the same as the name above
+  version = '0.1',
+  description = 'my description',
+  author = 'Alejandro Esquiva',
+  author_email = 'alejandro@geekytheory.com',
+  url='https://github.com/deivguerrero/friendly-config.git',
+  download_url='https://github.com/deivguerrero/friendly-config/tarball/0.0.1',
+  keywords = ['testing', 'logging', 'example'],
+  classifiers = [],
+)
